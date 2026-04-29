bca9ecbf89da341ef4d04def4c397563b34af1e3
Pari820 <saisojpm@gmail.com>
2026-04-28 20:44:19 -0500

    Delete lp1-u1-uoeu-master directory

--- lp1-u1-uoeu-master/src/main/java/com/example/demo/repository/AlumnoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Alumno;
-
-public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
-
-}
